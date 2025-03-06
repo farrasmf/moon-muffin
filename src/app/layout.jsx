1282e665266129aa7f9f37994edda057e027775a
HMID-Service <140302936+farrasmf@users.noreply.github.com>
2025-03-06 13:55:33 +0700

    Initial Commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,26 @@
+import {  Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+})
+
+export const metadata = {
+  title: "Moon Muffin",
+  description: "Moon Muffin by C+",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        <Header/>
+        {children}
+      </body>
+    </html>
+  );
+}
